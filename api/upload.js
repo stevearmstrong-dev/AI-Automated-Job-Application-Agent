eec668c32902951e831475a52fc3632b41088b1b
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2024-03-11 05:54:22 -0300

    Authentication with Google

--- api/upload.js
+++ api/upload.js
@@ -13,8 +13,25 @@ const { Storage } = require('@google-cloud/storage');
 //     credentials: serviceAccount,
 // });
 
-const storage = new Storage();
-const bucket = storage.bucket('resume-global');
+async function authenticateImplicitWithAdc() {
+    // This snippet demonstrates how to list buckets.
+    // NOTE: Replace the client created below with the client required for your application.
+    // Note that the credentials are not specified when constructing the client.
+    // The client library finds your credentials using ADC.
+    const storage = new Storage({
+      projectId,
+    });
+    const [buckets] = await storage.getBuckets();
+    console.log('Buckets:');
+  
+    for (const bucket of buckets) {
+      console.log(`- ${bucket.name}`);
+    }
+  
+    console.log('Listed all storage buckets.');
+  }
+  
+  authenticateImplicitWithAdc();
 
 const upload = multer({
     storage: multer.memoryStorage(),
