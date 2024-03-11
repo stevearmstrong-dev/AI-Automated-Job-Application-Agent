964625c9918775baf85ad7a7f1ed1c0f37348c88
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2024-03-11 03:58:30 -0300

    Design overheaul vercel deployment

--- api/upload.js
+++ api/upload.js
@@ -13,10 +13,18 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 module.exports = (req, res) => {
-    upload.single('resume')(req, {}, err => {
-        if (err) {
+    upload.single('resume')(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            console.error('Multer error:', err);
+            return res.status(500).send('Upload error');
+        } else if (err) {
+            console.error('Unknown error:', err);
             return res.status(500).send('Upload error');
         }
+
+        console.log('File uploaded:', req.file);
+        console.log('Form data:', req.body);
+
         res.redirect('/upload-success');
     });
 };
